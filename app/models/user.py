74a89ddad74b60214a12d5f5cde0facefdeb020a
Haasz Bence <haasz0.bence@gmail.com>
2026-06-11 17:45:54 +0200

    Add email_verification model

--- app/models/user.py
+++ app/models/user.py
@@ -1,7 +1,7 @@
 import uuid
 from datetime import datetime, timezone
 
-from sqlalchemy import String, Boolean, DateTime
+from sqlalchemy import Boolean, DateTime, String
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.database import Base
